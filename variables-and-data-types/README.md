7bb6d855b2301d519d3890a0346a5cebbdcf74e2
James Anievas <jameswilfred.anievas@gmail.com>
2026-01-16 00:54:19 +0800

    Learn data types

---
+++ variables-and-data-types/README.md
@@ -0,0 +1,17 @@
+Data Types
+
+<!-- What are data types? -->
+
+- Data types help us organise data in different categories.
+
+<!-- Different data types in JavaScript -->
+
+1. String- "Hello, welcome to JavaScript!"
+
+2. Number- 5, 6.5, 8, 100
+
+3. Boolean- true or false
+
+4. Undefined undefined
+
+5. Null null
---
+++ variables-and-data-types/index.js
@@ -0,0 +1,44 @@
+// #region Strings
+// Different ways to print:
+console.log("Hello World");
+console.log("Hello " + "World");
+
+// Printing specific character of a string (0, 1, 2, 3,4 ,5)
+console.log("Hello"[0]);
+console.log("Hello".length);
+
+// Creating a Variable
+let name = "James Anievas";
+console.log(name);
+console.log(name[0]);
+// -1 will always be equivalent to the last letter
+console.log(name[name.length - 1]);
+// #endregion
+
+// #region Numbers
+let num1 = 1;
+let num2 = 2;
+let sum = num1 + num2;
+console.log(num1 + "+" + num2 + "=" + sum);
+
+let result = 1 + 1;
+console.log(result);
+// #endregion Numbers
+
+// #region Boolean
+let example1 = 20 > 30;
+console.log(example1);
+// #endregion Boolean
+
+// #region Null & Undefined
+let example2 = null;
+console.log(example2);
+
+let example3 = undefined;
+console.log(example3);
+
+let example4 = "James" * 5;
+console.log(example4);
+// #endregion Null & Undefined
+
+// #region Variables
