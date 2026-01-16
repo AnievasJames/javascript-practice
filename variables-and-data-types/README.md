453cbfce900bb572714bd2b84c35ccbe2989224d
James Anievas <jameswilfred.anievas@gmail.com>
2026-01-17 00:03:13 +0800

    Learn comparison operators

--- variables-and-data-types/README.md
+++ variables-and-data-types/README.md
@@ -1,10 +1,12 @@
+<!-- ----------DATA TYPES ---------- -->
+
 Data Types
 
-<!-- What are data types? -->
+What are data types?
 
 - Data types help us organise data in different categories.
 
-<!-- Different data types in JavaScript -->
+Different data types in JavaScript:
 
 1. String- "Hello, welcome to JavaScript!"
 
@@ -15,3 +17,54 @@ Data Types
 4. Undefined undefined
 
 5. Null null
+
+<!-- ---------- VARIABLES ---------- -->
+
+Variables
+
+How do we create a variaable
+
+1. Let
+
+2. Const
+
+3. Var
+
+When to use let vs const in JavaScript:
+
+Use const (default choice):
+For variables that won't be reassigned
+Arrays and objects (even if you modify their contents)
+Function references
+Rule of thumb: Use this 90% of the time
+
+Use let:
+For variables that WILL be reassigned (counters, toggles, accumulating values)
+Loop counters (for loops)
+
+Quick example:
+const user = {} ✅ (object won't be reassigned, even if you change properties)
+
+let count = 0 ✅ (will be reassigned: count++, count = 5, etc.)
+
+<!-- ---------- COMPARISON OPERATORS ---------- -->
+
+Comparison Operators
+
+Compare two values and return true or false
+
+> greater than
+
+< less than
+
+> = greater than or equal to
+
+<= less than or equal to
+
+== equal values '5' == 5 is True
+
+=== equal values and types '5' === 5 is False (Recommended for type safety)
+
+!= checks for not equal values
+
+!== checks for not equal values and types (Recommended)
--- variables-and-data-types/index.js
+++ variables-and-data-types/index.js
@@ -1,4 +1,5 @@
 // #region Strings
+
 // Different ways to print:
 console.log("Hello World");
 console.log("Hello " + "World");
@@ -13,9 +14,11 @@ console.log(name);
 console.log(name[0]);
 // -1 will always be equivalent to the last letter
 console.log(name[name.length - 1]);
+
 // #endregion
 
 // #region Numbers
+
 let num1 = 1;
 let num2 = 2;
 let sum = num1 + num2;
@@ -23,14 +26,18 @@ console.log(num1 + "+" + num2 + "=" + sum);
 
 let result = 1 + 1;
 console.log(result);
+
 // #endregion Numbers
 
 // #region Boolean
+
 let example1 = 20 > 30;
 console.log(example1);
+
 // #endregion Boolean
 
 // #region Null & Undefined
+
 let example2 = null;
 console.log(example2);
 
@@ -39,6 +46,53 @@ console.log(example3);
 
 let example4 = "James" * 5;
 console.log(example4);
+
 // #endregion Null & Undefined
 
 // #region Variables
+
+// Let Keyword
+let userEmail = "james@gmail.com";
+let age = 21;
+let userName = "James";
+
+age = age + 1;
+
+// Const Keyword
+const points = 100;
+
+// points = 50 (This won't work)
+//  We can never reassign a const variable
+
+console.log(points);
+
+// #endregion Variables
+
+//  #region Currency Converter Exercise
+
+//$AUD to $USD Formula:
+
+//  AUD = USD x 1.5
+
+let USD = 1200;
+let AUD = USD * 1.5;
+
+console.log(AUD);
+
+//  #endregion Currency Converter Exercise
+
+// #region Comparison Operators
+
+let comparison1 = 5 == "5"; // == should have the same value
+console.log(comparison1);
+
+let comparison2 = 5 === "5"; // === should have the same value and type
+console.log(comparison2); // === is good for type safety
+
+let comparison3 = 5 !== "5";
+console.log(comparison3);
+
+let comparison4 = 5 != "5";
+console.log(comparison4);
+
+// #endregion Comparison Operators
