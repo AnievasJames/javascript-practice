6d19963b0f77a50a07c5732a5bc0bcd7cd9a2528
James Anievas <jameswilfred.anievas@gmail.com>
2026-01-18 23:52:46 +0800

    Learn while loop

---
+++ loops/README.md
@@ -0,0 +1,11 @@
+<!-- ---------- LOOPS ---------- -->
+
+What do we use loops for?
+
+Loops repeat the same code over and over again.
+
+Types of loops
+
+- for loop (MOST USED)
+- while loop
+- do while loop
---
+++ loops/index.js
@@ -0,0 +1,14 @@
+//  DRY- Don't repeat yourself
+
+// #region while loop
+
+let counter = 1;
+
+while (counter <= 20) {
+  console.log(counter);
+  counter = counter + 1;
+}
+
+console.log("while loop finished running");
+
+// #endregion while loop
