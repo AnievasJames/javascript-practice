f829ae456d24c13703072bc2ebf313c3806abbb2
James Anievas <jameswilfred.anievas@gmail.com>
2026-01-20 00:37:07 +0800

    Learn for loop

--- loops/README.md
+++ loops/README.md
@@ -9,3 +9,19 @@ Types of loops
 - for loop (MOST USED)
 - while loop
 - do while loop
+
+For Loop
+
+for ( let i = 0; i < 20; i++) {
+
+     console.log(i)
+
+}
+
+Create a variable "i"
+
+Set the variable "i" to 0
+
+Set the variable "i" less than how many times we want to loop (20)
+
+Increment the variable "i" by 1 (i++)
--- loops/index.js
+++ loops/index.js
@@ -4,11 +4,40 @@
 
 let counter = 1;
 
-while (counter <= 20) {
+while (counter <= 5) {
   console.log(counter);
   counter = counter + 1;
+  // counter++; (DIFFERENT METHOD)
 }
 
 console.log("while loop finished running");
 
 // #endregion while loop
+
+// #region for loop
+
+// for (let i = 0; i < 5; i = i + 1) (DIFFERENT METHOD)
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+
+  // console.log(i + 1) //(USE TO START FROM 1)
+}
+
+// #endregion for loop
+
+// #region exercise
+//  This includes data types, variables, conditionals and loops
+
+// Write a for-loop that loops through 1 to 15.
+// If the number is divisible by 5, Print "ASAP Frontend"
+
+// Example
+// 1 - "ASAP"
+// 2 - "Frontend"
+// 3 - "ASAP"
+// 4 - "Frontend"
+// 5 - "ASAP Frontend"
+// ...
+
+// #endregion exercise
