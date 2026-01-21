deb63b68263390abb717f22ce4d4e1c19fa34863
James Anievas <jameswilfred.anievas@gmail.com>
2026-01-22 02:50:55 +0800

    Add loop exercise to print text character by character

--- loops/index.js
+++ loops/index.js
@@ -52,7 +52,6 @@ for (let i = 0; i < 5; i++) {
 
 for (let result = 1; result <= 15; ++result) {
   if (result % 5 === 0) {
-    console.log(`${result} - ASAP Frontend`);
   } else if (result % 2 === 0) {
     console.log(`${result} - Frontend`);
   } else {
@@ -61,3 +60,36 @@ for (let result = 1; result <= 15; ++result) {
 }
 
 // #endregion exercise
+
+// #region exercise 2
+
+for (let result2 = 1; result2 <= 13; ++result2) {
+  if (result2 === 1) {
+    console.log(`${result2} - A`);
+  } else if (result2 === 2) {
+    console.log(`${result2} - S`);
+  } else if (result2 === 3) {
+    console.log(`${result2} - A`);
+  } else if (result2 === 4) {
+    console.log(`${result2} - P`);
+  } else if (result2 === 5) {
+    console.log(`${result2} -  `);
+  } else if (result2 === 6) {
+    console.log(`${result2} - F`);
+  } else if (result2 === 7) {
+    console.log(`${result2} - r`);
+  } else if (result2 === 8) {
+    console.log(`${result2} - o`);
+  } else if (result2 === 9) {
+    console.log(`${result2} - n`);
+  } else if (result2 === 10) {
+    console.log(`${result2} - t`);
+  } else if (result2 === 11) {
+    console.log(`${result2} - e`);
+  } else if (result2 === 12) {
+    console.log(`${result2} - n`);
+  } else if (result2 === 13) {
+    console.log(`${result2} - d`);
+  }
+}
+// #endregion exercise 2
